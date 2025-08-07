93f08d96d3809e2bd356719c6cfb5e058fd6d2e9
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2025-08-07 09:09:34 +0545

    Not a real only testing purpose

--- testing.php
+++ testing.php
@@ -1,7 +1,10 @@
+// testing welcome msg
+
 <?php
 echo "welcome to our testing running classes!"; # testing msg
 ?>
 
+// value & veriable testing 
 <?php
 $name = "Rkn Developer";
 $age = 21;
@@ -14,4 +17,24 @@ echo "Age: $age\n";
 echo "Height: $height\n";
 echo "Active User: $is_active\n";
 echo "all team: $team\n";
+?>
+
+// maths 
+<?php
+$a = 10;
+$b = 3;
+
+echo $a + $b;  // 13
+echo "\n";
+echo $a - $b;  // 7
+echo "\n";
+echo $a * $b;  // 30
+echo "\n";
+echo $a / $b;  // 3.3333
+echo "\n";
+echo $a % $b;  // 1
+echo "\n";
+
+$a += 5;       // $a = $a + 5 = 15
+echo $a;       // 15
 ?>
