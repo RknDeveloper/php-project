6ba80466a099ba3e75f1e0e26ff640066369115a
Rkn Developer <138338185+RknDeveloper@users.noreply.github.com>
2025-08-07 08:55:17 +0545

    Testing.php

---
+++ testing.php
@@ -0,0 +1,17 @@
+<?php
+echo "welcome to our testing running classes!"; # testing msg
+?>
+
+<?php
+$name = "Rkn Developer";
+$age = 21;
+$height = 5.9;
+$is_active = true;
+$team = 5;
+
+echo "Name: $name\n";
+echo "Age: $age\n";
+echo "Height: $height\n";
+echo "Active User: $is_active\n";
+echo "all team: $team\n";
+?>
